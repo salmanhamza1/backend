1d9aa7d49a1e241588da6a2715476dfba08e37d7
salmanhamza1 <125406541+salmanhamza1@users.noreply.github.com>
2023-02-19 23:21:42 +0500

    update

--- server/database/db.js
+++ server/database/db.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const database = process.env.DB;
+
 const Connection = async () => {
   try {
-    mongoose.connect(process.env.DB, {
+    mongoose.connect(database, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
